9932a7eec77f7b824c8fdd62cc477c2c791a1e2e
Viathorr <142295754+Viathorr@users.noreply.github.com>
2024-11-07 16:44:44 +0200

    Added main screen UI, added custom btn background and animations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.echowise"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.echowise"
--- app/src/main/java/com/example/echowise/MainActivity.kt
+++ app/src/main/java/com/example/echowise/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.echowise
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var responseLog: TextView
+    private lateinit var instructionTextView: TextView
+
+    private var dotCount = 0
+    private val dots = "..."
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        responseLog = findViewById(R.id.responseLog)
+        instructionTextView = findViewById(R.id.instruction)
+
+        startTextViewAnimations()
+    }
+
+    private fun startTextViewAnimations() {
+        val fadeAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.dots_animation)
+        instructionTextView.startAnimation(fadeAnimation)
+
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = object : Runnable {
+            @SuppressLint("SetTextI18n")
+            override fun run() {
+                if (dotCount < 3) {
+                    responseLog.text = "Waiting for command" + dots.substring(0, ++dotCount)
+                } else {
+                    dotCount = 0
+                    responseLog.text = "Waiting for command"
+                }
+                handler.postDelayed(this, 500)
+            }
         }
+        handler.post(runnable)
     }
 }
